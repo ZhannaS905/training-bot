845dbcd239a519ea2c43e4d6c90a05452e05ccb7
ZhannaS905 <82699712+ZhannaS905@users.noreply.github.com>
2026-09-26 17:28:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 // bot.js - УПРОЩЕННАЯ ВЕРСИЯ: ТОЛЬКО ОПРОСЫ, ЗАПИСЬ И РАСПИСАНИЕ
 require('dotenv').config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const { Bot, Keyboard } = require('@maxhub/max-bot-api');
 
 const bot = new Bot(process.env.BOT_TOKEN);
